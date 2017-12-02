a7a1cd01442cbd5a96634c1b556ec862f0809db8
Ashley <github@victorianfox.com>
2017-12-02 17:43:06 +0100

    Initial rest stuff

--- api/users.go
+++ api/users.go
@@ -1,1 +1,28 @@
 package api
+
+import (
+	"core/types"
+	"github.com/emicklei/go-restful"
+	"net/http"
+)
+
+type UserResource struct {
+	users map[string]types.User
+}
+
+func Register(container *restful.Container) {
+	u := UserResource{map[string]types.User{}}
+	u.Register(container)
+}
+
+func (u *UserResource) Register(container *restful.Container) {
+	ws := new(restful.WebService)
+	ws.Path("/user")
+	ws.Route(ws.GET("").To(u.listUsers))
+
+	container.Add(ws)
+}
+
+func (u *UserResource) listUsers(request *restful.Request, response *restful.Response) {
+	response.WriteErrorString(http.StatusNotFound, "User could not be found.")
+}
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/emicklei/go-restful"
 	"github.com/golang/glog"
+	"io"
+	"net/http"
+	"core/api"
 )
 
 var VERSION = "Unknown-build"
@@ -18,6 +21,12 @@ func main() {
 	restful.DefaultResponseContentType(restful.MIME_JSON)
 	restful.DefaultContainer.EnableContentEncoding(true)
 	restful.DefaultContainer.Router(restful.CurlyRouter{})
-	
 
+	api.Register(restful.DefaultContainer)
+
+	glog.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func hello(req *restful.Request, resp *restful.Response) {
+	io.WriteString(resp, "world")
 }
